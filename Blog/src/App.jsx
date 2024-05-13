f38a77ff90c89855878809f5facad6f00d6466ee
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-05-12 23:46:07 -0600

    Actualizacion del CSS

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,37 +1,45 @@
 import jojo from './assets/Jojo-removebg-preview.png'
+import logo from './assets/img/Recurso 2-8.png'
 import './App.css'
 
 function App() {
 
   return (
     <div className='Body'>
-      <div>
-          <nav class="menu">
-            <ul>
-                <li><button className='button' href="#projects">Projects</button></li>
-                <li><button className='button' href="#info">Info</button></li>
-                <li><button className='button' href="#contact">Contact</button></li>
-            </ul>
-        </nav>
-        <br />
-        <br />
-    </div>
       <div class="contenedor">
         <header class="encabezado">
           <p>Este portafolio no era tuyo! ¡Era mio!</p>
           <h1>Sebastian Ramirez <img className='jojo' src={jojo} alt="jojo" /></h1>
         </header>
+        <nav class="menu">
+            <ul>
+                <li><button className='button' href="#projects">Projects</button></li>
+                <li><button className='button' href="#info">Info</button></li>
+                <li><button className='button' href="#contact">Contact</button></li>
+            </ul>
+            <br />
+            <br />
+            <br />
+        </nav>
+    </div>
+    <div className='contenedor-titulo'>
+      <h1>SOBRE MI</h1>
     </div>
       <div className='ContenedorForm'>
         <form action="" className='Cuadrado'>
+          <form action="" className='Cuadrado-mini'>
           <p class="pie-de-pagina">
-            <p>Hola, soy Sebastian Ramirez Giron (Mis amigos me conocen como Warty) y soy un desarrollador web principiante que aspira a siempre mejorar en todos los aspectos posibles <img src="5" alt="" /></p>
-          </p>
+            <p>Hola, soy Sebastian Ramirez Giron (Mis amigos me conocen como Warty) y soy un desarrollador web principiante que aspira a siempre mejorar en todos los aspectos posibles.</p>
+          </p> 
+          </form>
+          <img className='img-block' src={logo} alt="" />
         </form>
         <form action="" className='Cuadrado'>
+          <form action="" className='Cuadrado-mini'>
           <p className='pie-de-pagina'>
-            <p>Los idiomas de programacion que suelo manejar son </p>
+            <p>Los idiomas informaticos que mejor manejo son JavaScript, MongoDB, HTML5, CSS, REACT, MySql etc, sin embargo no dejo la posibilidad de aprender otro idioma informatico en un futuro.</p>
           </p>
+          </form>
         </form>
       </div>
       <div>
